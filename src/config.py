a452201858528c66603487bd91092281296fb06c
dargor980 <german.contrerasa@utem.cl>
2023-08-07 23:55:05 -0400

    dependencias

--- src/config.py
+++ src/config.py
@@ -1,2 +1,13 @@
 from dataclasses import dataclass
 from pathlib import Path
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+@dataclass
+class ConfigEmail:
+    email_user: str = os.getenv("EMAIL_USER")
+    email_pass: str = os.getenv("EMAIL_PASSWORD")
+    imap_server: str = os.getenv("IMAP_SERVER")
